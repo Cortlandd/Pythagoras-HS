b0080e14e17eaaf33a3ae644d423e008f5d84680
Cortland Walker <cortland1568@gmail.com>
2016-06-12 14:39:58 -0500

    Thanks You, Chris Allen

--- src/Pythagoras.hs
+++ src/Pythagoras.hs
@@ -1,21 +1,15 @@
--- All functions will be contained in an import called: Pythagoras
+-- All functions will be contained in a module called: Pythagoras
 module Pythagoras where {- a² + b² = c² -}
 
--- > solveForA 9 15
--- 12.0
--- FINDING FIX. Want Whole Numbers
--- Also hopfully get decimals to round up.
-
--- Using Factorial instead of Num because decimal numbers.
-solveForA :: (Floating a, Fractional a, Ord a) => a -> a -> a
+solveForA :: (Floating a, Ord a) => a -> a -> a
 solveForA b c
-	| b > c 		= error "The hyptenuse, c, is always larger than the legs, a and b."
-	| otherwise = sqrt (c^2 - b^2)
+  | b > c     = error "The hypotenuse c is always larger than the legs a and b."
+  | otherwise = sqrt (c^2 - b^2)
 
-solveForB :: (Floating a, Fractional a, Ord a) => a -> a -> a
+solveForB :: (Floating a, Ord a) => a -> a -> a
 solveForB a c 
-	| a > c 		= error "The hyptenuse, c, is always larger than the legs, a and b." 
-	| otherwise = sqrt (c^2 - a^2) 
+  | a > c     = error "The hypotenuse c is always larger than the legs a and b." 
+  | otherwise = sqrt (c^2 - a^2) 
 
-solveForC :: (Floating a, Fractional a) => a -> a -> a
+solveForC :: (Floating a) => a -> a -> a
 solveForC a b = sqrt (a^2 + b^2)
